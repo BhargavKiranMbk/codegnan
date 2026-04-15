50453c9eacda980534c95a8efc6ec7babf84b3f4
Bhargav  Kiran Mutchu <bhargavkiran.mbk@gmail.com>
2026-04-15 10:51:18 +0530

    Add files via upload
    1 day of codegnan

---
+++ day1.py
@@ -0,0 +1,46 @@
+'''print("this is my first day at coding")
+
+---> what is python?
+
+
+ 
+---> Python is a high-level language means no need to worry about low-level managment like memory allocation.
+
+
+---> Python never save anything perminantly, it will only save at run time.
+
+a=0
+b=9
+print(id(a))
+print(id(b))
+
+
+---> python supports OOP'S-(Object oriented programming language).
+
+a = int(input("enter A number: "))
+b = int(input("enter B number: "))
+print(a+b)
+
+print(f"b = {b}")
+print(f"a = {a}")
+
+
+---> python is also called dynamically typed language means no need to mention the type od data your in a variable.
+
+a=9
+b="python"
+print(type(a))
+print(type(b))
+
+---> python also is called as interpered language its means the python code will excutes line by line process
+
+a=9
+  b="python"
+  print(type(a))
+  print(type(b))
+  
+---> syntax less language
+'''
+
+
+
