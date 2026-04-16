ef15c6601886b4b70882183af323cb385b37354e
Bhargav  Kiran Mutchu <bhargavkiran.mbk@gmail.com>
2026-04-16 10:32:35 +0530

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,98 @@
+'''
+
+--->Variable
+
+      in programing a variable is a container which can hold data
+      
+  to define variables  we have 2 rules
+ 
+ 1. good rule to define  a varaible(no errors)
+ 
+ example:
+ 
+ A=90
+print(A)
+
+N=89
+print(N)
+
+_f=200+1
+print(_f)
+
+ 2. bad rule to define variable (will get error)
+ if we use special chareters we will get errors
+
+ example:
+ 
+ @n=7
+print(@n)
+
+-m=90
+print(-m)
+
+---> we are going to use meaningfull words or names  for defining variables
+_num = 90
+
+num_2 = 201
+
+print(_num+num_2)
+
+9 = 10
+print(9)
+
+--->Keywords
+    key words not gonna use as a variables they are 32 key words
+    identifiers are not but keywords are reserved words
+    example if,if-else,while,do-while,forloop,
+
+a,b,c = 25, 26,56
+print(a,b,c)
+print(b)
+print(c)
+print(a)
+print(b+c)
+
+token : a token is a small program or code in python that divides multiple tasks
+
+Comment:  they are 2 types of commands they are
+
+ single-line comment    # has is used for single line comment -->   #kfmdkmfdkfmlkdmf
+ multi-line comment     ''' ''' or """ """    anything by in the middle of the comments it will not  considerer
+
+Boolen type:
+
+---> hence the boolen is used to find true or false
+
+num1 = 10
+num2 = 30
+print(num1==num2)
+
+num = 7
+if num%2==0:
+    print(f"{num} is even number")
+else:
+    print(f"{num} odd")
+
+
+ 
+ '''
+          
+    
+num = 7
+if num%2==0:
+    print(f"{num} is even number")
+else:
+    print(f"{num} odd")
+
+
+
+
+
+
+
+
+
+
+
+
+
